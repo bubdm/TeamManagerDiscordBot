eaf2a8a990986a54443e21195893d372b6065c7e
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-02-02 06:08:37 -0500

    Add files via upload

---
+++ TeamManagerDiscordBotPR/Program.cs
@@ -0,0 +1,13 @@
+﻿using DiscordBot;
+
+namespace TeamManagerDiscordBotPR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();                                //creates instance of bot
+            bot.RunAsync().GetAwaiter().GetResult();            //runs asynchronously after confirming bot token
+        }
+    }
+}
